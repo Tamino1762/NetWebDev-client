08edd2ff2c07b42b385d62762023ae5d81248c62
Tamino1762 <paganinifreak@gmail.com>
2020-02-05 21:21:34 -0600

    Just about done

--- wwwroot/js/balloons.js
+++ wwwroot/js/balloons.js
@@ -1,3 +1,83 @@
-﻿$(function () {
+﻿var animate = ["bounce", "jello", "shake", "flash", "tada"];
+var randomBalloon = animate[Math.floor(Math.random() * 5)];
+$('h1.animated').addClass(randomBalloon);
+
+$(function() {
     $('#birthday').pickadate({ format: 'mmmm, d' });
+
+    // uncheck all checkboxes (FireFox)
+    $('.form-check-input').each(function() {
+        $(this).prop('checked', false);
+    });
+    // event listener for check/uncheck
+    $('.form-check-input').on('change',
+        function() {
+            // make the image visible
+            $('#' + this.id + 'Img').css('visibility', 'visible')
+            // animate balloon in/out based on checkbox
+            $(this).is(':checked')
+                ? $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown')
+                : $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+        });
+
+    $('#submit').on('click',
+        function(e) {
+            // preload audio
+            var toast = new Audio('media/toast.wav');
+            var checkbox = 0;
+            $('.form-check-input').each(function(e) {
+                if ($(this).prop('checked') == true) {
+                    checkbox = checkbox + 1;
+                }
+            });
+            if (checkbox == 0) {
+                e.preventDefault();
+                // first pause the audio (in case it is still playing)
+                toast.pause();
+                // reset the audio
+                toast.currentTime = 0;
+                // play audio
+                toast.play();
+                $('#toast').toast({ autohide: false }).toast('show');
+            }
+
+        });
+
+    $(document).on('keyup',
+        function(e) {
+            if (e.key === "Escape") {
+                $('#toast').toast('hide');
+            }
+        });
+    $('#boxes').on('click',
+        function() {
+
+            $('.form-check-input').each(function(e) {
+                $(this).prop('checked', true);
+                $('#' + this.id + 'Img').css('visibility', 'visible');
+                // animate balloon in/out based on checkbox
+                $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown');
+            });
+
+        });
+    $('#noBoxes').on('click',
+        function() {
+
+            $('.form-check-input').each(function(e) {
+                $(this).prop('checked', false);
+                $('#' + this.id + 'Img').css('visibility', 'visible');
+                // animate balloon in/out based on checkbox
+                $('#' + this.id + 'Img').removeClass().addClass('animated bounceOutUp');
+            });
+
+        });
+
+    $('.form-check-label').mouseover(function () {
+        $('#change').css("color", $(this).data('color'));
+    });
+    $('.form-check-label').mouseleave(function () {
+        $('#change').css("color", 'black');
+    });
 });
+
+
